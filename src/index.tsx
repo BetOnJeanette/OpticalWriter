9054127957076b85c7c86eb48ca2be493b54f487
BetOnJeanette <jeanettecrull99@gmail.com>
2025-08-05 22:26:19 -0400

    Implement isolation pattern

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@
 import { render } from "solid-js/web";
 import App from "./App";
 
+window.__TAURI_ISOLATION_HOOK__ = (payload) => {
+    return payload;
+}
+
 render(() => <App />, document.getElementById("root") as HTMLElement);
