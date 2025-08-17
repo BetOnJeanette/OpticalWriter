ac8317d910568af3467851c4edc7eedf7f55770a
BetOnJeanette <jeanettecrull99@gmail.com>
2025-08-17 17:23:46 -0400

    Move the isolation hook into its own file

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@
 import { render } from "solid-js/web";
 import App from "./App";
 
-window.__TAURI_ISOLATION_HOOK__ = (payload) => {
-    return payload;
-}
+
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
