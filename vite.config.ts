9afef1c451dbe5fa5f9c3d312a55d0f35429c9ef
BetOnJeanette <jeanettecrull99@gmail.com>
2025-08-17 17:51:29 -0400

    Install SUID

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import suidPlugin from "@suid/vite-plugin";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [solid()],
+  plugins: [suidPlugin(), solid()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
