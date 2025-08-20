030873398cabfb6198e5d436ef41e47f024a429e
BetOnJeanette <jeanettecrull99@gmail.com>
2025-08-20 00:16:18 -0400

    Remove print statements

--- src-tauri/src/commands/projectBin.rs
+++ src-tauri/src/commands/projectBin.rs
@@ -5,11 +5,9 @@ use tauri::{ AppHandle, Runtime };
 
 #[tauri::command]
 pub fn request_new_file<R: Runtime>(app_handle: AppHandle<R>) {
-    println!("Openning dialog");
     app_handle.dialog()
         .file()
         .pick_files( move |picked_files| { 
-            println!("adding files {}", picked_files.iter().len());
             let Some(safe_files) = picked_files else { return };
             if safe_files.len() == 0 { return; }
             project_bin_data::add_files_to_bin(safe_files, app_handle);
