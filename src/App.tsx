4dbb0ee93c3ec43a170c07a4a2cc0467f396c9c0
BetOnJeanette <jeanettecrull99@gmail.com>
2025-08-19 23:55:10 -0400

    Remove greet function

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { createSignal, JSXElement } from "solid-js";
-import { invoke } from "@tauri-apps/api/core";
+import { JSXElement } from "solid-js";
 import "./App.css";
 import { ProjectBin } from "./widgets/ProjectBin/ProjectBin";
 
 function App(): JSXElement {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
-
   return (
     <main class="container">
       <ProjectBin />
