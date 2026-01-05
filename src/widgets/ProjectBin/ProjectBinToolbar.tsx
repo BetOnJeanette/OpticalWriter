26421fc6a733b5ed71e10e7043688881f1ec8e98
BetOnJeanette <jeanettecrull99@gmail.com>
2026-01-04 20:00:21 -0500

    Add button for Adding folder

--- src/widgets/ProjectBin/ProjectBinToolbar.tsx
+++ src/widgets/ProjectBin/ProjectBinToolbar.tsx
@@ -1,6 +1,7 @@
 import { JSXElement } from "solid-js";
 import { Button } from "@suid/material";
 import { invoke } from "@tauri-apps/api/core";
+import { AiFillFolderAdd } from "solid-icons/ai"
 
 interface ProjectBinToolbarProps {
 
@@ -9,5 +10,6 @@ interface ProjectBinToolbarProps {
 export function ProjectBinToolbar({}: ProjectBinToolbarProps): JSXElement {
     return (<> 
         <Button size="small" fullWidth={false} onClick={() => invoke("request_new_file")}>+</Button>
+        <Button size="small" fullWidth={false}><AiFillFolderAdd /></Button>
     </>)
 }
