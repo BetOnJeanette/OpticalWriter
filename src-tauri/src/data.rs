8d708d2e23635045091b9a1fd8398dace9668549
BetOnJeanette <jeanettecrull99@gmail.com>
2026-02-27 11:12:03 -0500

    Actually init the state for the directories

--- src-tauri/src/data.rs
+++ src-tauri/src/data.rs
@@ -3,5 +3,6 @@ pub mod project_bin;
 pub mod project_bin_dir;
 
 pub fn prep_state(man: &AppHandle){
-    project_bin::init_state(&man); 
+    project_bin::init_state(man); 
+    project_bin_dir::init_state(man);
 }
