288c01e779ac8e5139e5a1c87a23b8a257635c6d
BetOnJeanette <jeanettecrull99@gmail.com>
2026-03-09 18:23:50 -0400

    Remove console log for the files

--- src/widgets/ProjectBin/ProjectBinDirectory.tsx
+++ src/widgets/ProjectBin/ProjectBinDirectory.tsx
@@ -1,4 +1,3 @@
-import { listen } from "@tauri-apps/api/event";
 import { JSXElement, Setter } from "solid-js";
 import { ProjectFolderContents } from "./ProjectBinFolder";
 import { ROOT_KEY } from "./ProjectBin";
@@ -7,13 +6,6 @@ interface ProjectBinDirectoryProps {
     curSelDirSetter: Setter<string>;
 }
 
-type NewFiles = {
-    files: string[];
-}
-
-
 export function ProjectBinDirectory({curSelDirSetter}: ProjectBinDirectoryProps): JSXElement {
-    listen<NewFiles>("file-added", (event) => console.log(event.payload));
-
     return (<ProjectFolderContents id={ROOT_KEY} curSelDirSestter={curSelDirSetter}/>)
 }
