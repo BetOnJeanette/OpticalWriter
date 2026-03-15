7ce2c4608b696041780074744c3e6414577be1ed
BetOnJeanette <jeanettecrull99@gmail.com>
2026-03-15 18:55:26 -0400

    Give the icon for the add folder button an accessible title

--- src/widgets/ProjectBin/ProjectBinToolbar.tsx
+++ src/widgets/ProjectBin/ProjectBinToolbar.tsx
@@ -9,6 +9,7 @@ type ProjectBinToolbarProps = {
 
 export const NEW_FILE_COMMAND = "request_new_file";
 export const NEW_FOLDER_COMMAND = "add_folder";
+export const NEW_FOLDER_TITLE = "Create Folder in Bin";
 
 export function ProjectBinToolbar({
 	curSelFold,
@@ -32,7 +33,7 @@ export function ProjectBinToolbar({
 					invoke(NEW_FOLDER_COMMAND, { selectedDirId: curSelFold() })
 				}
 			>
-				<AiFillFolderAdd />
+				<AiFillFolderAdd title={NEW_FOLDER_TITLE} />
 			</Button>
 		</>
 	);
