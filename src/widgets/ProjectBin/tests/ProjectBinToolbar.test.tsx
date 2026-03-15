542106040e5ec0e64f6610a89c7ccf8eb5ebabf2
BetOnJeanette <jeanettecrull99@gmail.com>
2026-03-15 18:57:15 -0400

    Create test to make sure that the buttons invoke the right command

--- src/widgets/ProjectBin/tests/ProjectBinToolbar.test.tsx
+++ src/widgets/ProjectBin/tests/ProjectBinToolbar.test.tsx
@@ -3,30 +3,58 @@ import { clearMocks, mockIPC } from "@tauri-apps/api/mocks";
 import { userEvent } from "@testing-library/user-event";
 import { afterEach, describe, expect, test, vi } from "vitest";
 import { ROOT_KEY } from "../ProjectBin";
-import { NEW_FILE_COMMAND, ProjectBinToolbar } from "../ProjectBinToolbar";
+import {
+	NEW_FILE_COMMAND,
+	NEW_FOLDER_COMMAND,
+	NEW_FOLDER_TITLE,
+	ProjectBinToolbar,
+} from "../ProjectBinToolbar";
 
 const user = userEvent.setup();
 
-afterEach(() => {
-	clearMocks();
-});
+function invokeSpy() {
+	return vi.spyOn(window.__TAURI_INTERNALS__, "invoke");
+}
 
 describe("Make sure the buttons function as expected", () => {
+	afterEach(() => {
+		clearMocks();
+	});
+
 	test.each([
 		ROOT_KEY,
 		"aNestedFolderId",
 	])("calls to add file at %s", async (currentFolder: string) => {
-		mockIPC(() => {}, { shouldMockEvents: true });
+		mockIPC(() => {});
 		const { getByRole } = render(() => (
 			<ProjectBinToolbar curSelFold={() => currentFolder} />
 		));
 		const addFileButton = getByRole("button", { name: "+" });
-		const spy = vi.spyOn(window.__TAURI_INTERNALS__, "invoke");
+		const spy = invokeSpy();
 		await user.click(addFileButton);
 		expect(spy).toBeCalledWith(
 			NEW_FILE_COMMAND,
 			{
 				selectedFolderId: currentFolder,
+			},
+			undefined,
+		);
+	});
+	test.each([
+		ROOT_KEY,
+		"aNestedFolderId",
+	])("calls to add folder at %s", async (currentFolder: string) => {
+		mockIPC(() => {});
+		const { getByRole } = render(() => (
+			<ProjectBinToolbar curSelFold={() => currentFolder} />
+		));
+		const addFolderButton = getByRole("button", { name: NEW_FOLDER_TITLE });
+		const spy = invokeSpy();
+		await user.click(addFolderButton);
+		expect(spy).toBeCalledWith(
+			NEW_FOLDER_COMMAND,
+			{
+				selectedDirId: currentFolder,
 			},
 			undefined,
 		);
