e0219866393e98b57a98d9374e8381065108bae5
BetOnJeanette <jeanettecrull99@gmail.com>
2026-05-10 22:40:08 -0400

    Finish parent folder test

--- src/widgets/ProjectBin/tests/projectBinFolderContents.test.tsx
+++ src/widgets/ProjectBin/tests/projectBinFolderContents.test.tsx
@@ -1,8 +1,13 @@
 import { render } from "@solidjs/testing-library";
+import { emit } from "@tauri-apps/api/event";
 import { clearMocks, mockIPC } from "@tauri-apps/api/mocks";
-import { afterAll, beforeAll, describe, test, vi } from "vitest";
+import { afterAll, beforeAll, describe, expect, test, vi } from "vitest";
 import { ROOT_KEY } from "../ProjectBin";
-import { ProjectBinFolderContents } from "../ProjectBinFolderContents";
+import {
+	type FolderCreated,
+	NEW_FOLDER_RECEIVED,
+	ProjectBinFolderContents,
+} from "../ProjectBinFolderContents";
 
 describe("The folders listen to events from the backend", () => {
 	beforeAll(() => {
@@ -13,13 +18,26 @@ describe("The folders listen to events from the backend", () => {
 		clearMocks();
 	});
 
-	test("New subfolders get added", () => {
-		const { getByRole } = render(() => {
-			<ProjectBinFolderContents
-				id={ROOT_KEY}
-				curSelDirSestter={vi.fn((_) => 1)}
-				Expanded={() => true}
-			/>;
+	test("New subfolders get added to its parent folder", () => {
+		const { getByText } = render(() => {
+			return (
+				<ProjectBinFolderContents
+					id={ROOT_KEY}
+					curSelDirSestter={vi.fn((_) => 1)}
+					Expanded={() => true}
+				/>
+			);
 		});
+        const NEW_FOLDER_LABEL = "NewFolder"
+		emit<FolderCreated>(NEW_FOLDER_RECEIVED, {
+			newFolder: {
+				displayName: NEW_FOLDER_LABEL,
+				containedFolders: [],
+				containedSources: [],
+			},
+			id: "awawawa",
+			parentId: ROOT_KEY,
+		});
+        expect(getByText(NEW_FOLDER_LABEL)).toBeInTheDocument();
 	});
 });
