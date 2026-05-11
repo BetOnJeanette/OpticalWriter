78db58ccf05d8aceeb5c4588d90c1b3e438db70f
BetOnJeanette <jeanettecrull99@gmail.com>
2026-05-11 18:02:43 -0400

    Test that files and folders are actually hidden when its parent collapses

--- src/widgets/ProjectBin/tests/ProjectFolder.test.tsx
+++ src/widgets/ProjectBin/tests/ProjectFolder.test.tsx
@@ -1,12 +1,42 @@
-import { render } from "@solidjs/testing-library";
+import { getByText, render } from "@solidjs/testing-library";
 import { mockIPC } from "@tauri-apps/api/mocks";
 import { userEvent } from "@testing-library/user-event";
 import { For } from "solid-js";
 import { beforeAll, describe, expect, test, vi } from "vitest";
 import { ROOT_KEY } from "../ProjectBin";
 import { ProjectFolder } from "../ProjectBinFolder";
+import { emit } from "@tauri-apps/api/event";
+import { NEW_FILE_RECEIEVED, NEW_FOLDER_RECEIVED } from "../ProjectBinFolderContents";
 
 const user = userEvent.setup();
+
+const NEW_FOLDER_LABEL = "NewFolder";
+const NEW_FILE_NAME = "NewFile";
+
+function GetTestFolder() {
+    const parent = ROOT_KEY;
+	return {
+		newFolder: {
+			displayName: NEW_FOLDER_LABEL,
+			containedFolders: [],
+			containedSources: [],
+		},
+		id: "awawawa",
+		parentId: parent,
+	};
+}
+
+function GetTestFile() {
+    const parent = ROOT_KEY;
+	return {
+		newFile: {
+			path: "/",
+			displayName: NEW_FILE_NAME,
+		},
+		id: "awawa",
+		parentId: parent,
+	};
+}
 
 const folders = [
 	{
@@ -18,6 +48,7 @@ const folders = [
 		name: "another folder",
 	},
 ];
+
 describe("Folders react to clicking for focus as expected", () => {
 	let rendered: ReturnType<typeof render> | undefined;
 	const curDirSetter = vi.fn((_) => 1);
@@ -49,8 +80,9 @@ describe("Folders react to clicking for focus as expected", () => {
 });
 
 describe("Folders react to other interactions as anticipated", () => {
-	test("Closing and opening the folder changes the icon", async () => {
-		const { getByRole } = render(() => (
+	test("Closing and opening the folder updates hidden state", async () => {
+        mockIPC(() => {}, { shouldMockEvents: true });
+		const { getByText, getByRole } = render(() => (
 			<For each={folders}>
 				{(folder, _) => (
 					<ProjectFolder
@@ -61,13 +93,19 @@ describe("Folders react to other interactions as anticipated", () => {
 				)}
 			</For>
 		));
+        
+        emit(NEW_FOLDER_RECEIVED, GetTestFolder());
+        emit(NEW_FILE_RECEIEVED, GetTestFile());
 
-		const expandFolderIcon = getByRole("img", {
-			name: `Expand ${folders[0].name}`,
-		});
+		const expandFolderIcon = getByRole("img", { name: `Expand ${folders[0].name}`, });
 
 		await user.click(expandFolderIcon);
+
+        const subFolderName = getByText(NEW_FOLDER_LABEL);
+        const fileName = getByText(NEW_FILE_NAME);
 		expect(expandFolderIcon).not.toBeInTheDocument();
+		expect(subFolderName).toBeInTheDocument();
+		expect(fileName).toBeInTheDocument();
 
 		const collapseFolderIcon = getByRole("img", {
 			name: `Collapse ${folders[0].name}`,
@@ -76,8 +114,8 @@ describe("Folders react to other interactions as anticipated", () => {
 
 		await user.click(collapseFolderIcon);
 		expect(collapseFolderIcon).not.toBeInTheDocument();
-		expect(
-			getByRole(`img`, { name: `Expand ${folders[0].name}` }),
-		).toBeInTheDocument();
+		expect(getByRole(`img`, { name: `Expand ${folders[0].name}` })).toBeInTheDocument();
+        expect(subFolderName).not.toBeInTheDocument();
+        expect(fileName).not.toBeInTheDocument();
 	});
 });
