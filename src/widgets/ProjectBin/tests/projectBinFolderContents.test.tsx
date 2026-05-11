b86cca453b121caaf59af2b3927932e0fa814f74
BetOnJeanette <jeanettecrull99@gmail.com>
2026-05-10 22:54:11 -0400

    Ensure that rendering does not interfere with each test

--- src/widgets/ProjectBin/tests/projectBinFolderContents.test.tsx
+++ src/widgets/ProjectBin/tests/projectBinFolderContents.test.tsx
@@ -1,7 +1,15 @@
-import { render } from "@solidjs/testing-library";
+import { cleanup, render, screen } from "@solidjs/testing-library";
 import { emit } from "@tauri-apps/api/event";
 import { clearMocks, mockIPC } from "@tauri-apps/api/mocks";
-import { afterAll, beforeAll, describe, expect, test, vi } from "vitest";
+import {
+	afterAll,
+	afterEach,
+	beforeAll,
+	describe,
+	expect,
+	test,
+	vi,
+} from "vitest";
 import { ROOT_KEY } from "../ProjectBin";
 import {
 	type FolderCreated,
@@ -10,12 +18,17 @@ import {
 } from "../ProjectBinFolderContents";
 
 describe("The folders listen to events from the backend", () => {
+	const NEW_FOLDER_LABEL = "NewFolder";
+
 	beforeAll(() => {
 		mockIPC(() => {}, { shouldMockEvents: true });
 	});
 
 	afterAll(() => {
 		clearMocks();
+	});
+	afterEach(() => {
+		cleanup();
 	});
 	test("New subfolders get added to its parent folder", async () => {
 		const { getByText } = render(() => {
@@ -27,7 +40,6 @@ describe("The folders listen to events from the backend", () => {
 				/>
 			);
 		});
-		const NEW_FOLDER_LABEL = "NewFolder";
 		emit<FolderCreated>(NEW_FOLDER_RECEIVED, {
 			newFolder: {
 				displayName: NEW_FOLDER_LABEL,
@@ -38,5 +50,27 @@ describe("The folders listen to events from the backend", () => {
 			parentId: ROOT_KEY,
 		});
 		expect(getByText(NEW_FOLDER_LABEL)).toBeInTheDocument();
+	});
+	test("New subfolders do not get added to other folders", async () => {
+		render(() => {
+			return (
+				<ProjectBinFolderContents
+					id={ROOT_KEY}
+					curSelDirSestter={vi.fn((_) => 1)}
+					Expanded={() => true}
+				/>
+			);
+		});
+		emit<FolderCreated>(NEW_FOLDER_RECEIVED, {
+			newFolder: {
+				displayName: NEW_FOLDER_LABEL,
+				containedFolders: [],
+				containedSources: [],
+			},
+			id: "awawawa",
+			parentId: "someOtherFolder",
+		});
+		const newFolder = screen.queryByText(NEW_FOLDER_LABEL);
+		expect(newFolder).not.toBeInTheDocument();
 	});
 });
